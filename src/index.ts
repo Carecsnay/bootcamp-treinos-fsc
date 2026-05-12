07363f8796ea3670d728e37ffebbfe0344aeda2a
Bruno Asus <carecsnay1@gmail.com>
2026-05-12 07:05:38 -0300

    ✨ (schema.prisma): add coverImageUrl field to WorkoutDay model ✨ (CreateWorkoutPlan.ts): introduce coverImageUrl to InputDto and OutputDto ✨ (CreateWorkoutPlan.ts): implement coverImageUrl in workout day creation ✨ (index.ts): log server docs url on startup

--- src/index.ts
+++ src/index.ts
@@ -138,6 +138,7 @@ app.route({
 
 try {
   await app.listen({ port: Number(process.env.PORT) || 3000 });
+  console.log("http://localhost:3000/docs");
 } catch (err) {
   app.log.error(err);
   process.exit(1);
